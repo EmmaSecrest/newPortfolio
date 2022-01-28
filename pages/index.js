3a2be5b2a58704006d2960e6c083b77f0ad4a1b8
Emma Secrest <emma@Emmas-MBP.attlocal.net>
2022-01-27 21:36:03 -0500

    Images added

--- pages/index.js
+++ pages/index.js
@@ -17,40 +17,32 @@ import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 // import {theme} from '../src/theme.js'
 import theme from '../src/theme'
-import Image from 'next/image'
+
 
             
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {  ThemeProvider } from '@mui/material/styles';
 
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="text.secondary" align="center">
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://mui.com/">
-//         Your Website
-//       </Link>{' '}
-//       {new Date().getFullYear()}
-//       {'.'}
-//     </Typography>
-//   );
-// }
+
+
 
 const cards = [
     { id: 1,
       name: 'Deep Dive',
       description: 'A Redux/React application that pulls information from different subreddits and displays their content. used React, Redux libraries for state management, using react hooks, image manipulation, network requests and deployment',
       gitLink: 'https://github.com/EmmaSecrest/redditApp',
-      image:'public/resources/deepDive.png'
+      image: '/deepDive.png'
       },
     { id: 2,
       name: 'Python Port scanner',
       description: 'A Python Code that will scan an IP address to find the ports open on that system',
-      gitLink: 'https://github.com/EmmaSecrest/portScan'
+      gitLink: 'https://github.com/EmmaSecrest/portScan',
+      image: '/portScanner.jpeg'
        },
     { id: 3,
       name: 'E-Commerce backend',
       description: 'used node and Express to create a server, boilerplate and routes with express, database creation with SQL along with some cloud computing with Heroku',
       gitLink: 'https://github.com/EmmaSecrest/ecommerce-app-rest-api',
+      image:'/ecommerse.png'
 
        }
     ];
@@ -106,8 +98,16 @@ In my free time, I enjoy cooking and gaming. I am also an amateur MUA!
                 <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                 >
-                  <Image src = {card.image} />
-                  
+                  <CardMedia
+                    component="img"
+                    sx={{
+                 
+                       // 16:9
+                      pt: '56.25%',
+                    }}
+                    image= {card.image}
+                    alt="random"
+                  />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
                       {card.name}
@@ -137,7 +137,8 @@ In my free time, I enjoy cooking and gaming. I am also an amateur MUA!
           color="text.secondary"
           component="p"
         >
-         esecrest908@gmail.com
+         esecrest908@gmail.com <br/>
+         <Link href = 'https://www.linkedin.com/in/emma-secrest/' >LinkedIn</Link >
         </Typography>
        
       </Box>
